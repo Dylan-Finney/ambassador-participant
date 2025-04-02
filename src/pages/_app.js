3e2dc8c4799197de148d783417edaad428459d28
Dylan Finney <54768129+Dylan-Finney@users.noreply.github.com>
2025-04-02 18:47:56 +0100

    Chakra Template

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
-import "@/styles/globals.css";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <ThemeProvider attribute="class" disableTransitionOnChange>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ChakraProvider>
+  );
 }
